6c3b032ba0a89d06b663065bb763cef53d8cb1f8
Michael Kanis <mks@skweez.net>
2011-04-22 16:19:02 +0200

    Add Overlay class

---
+++ src/net/skweez/geoclipse/map/Overlay.java
@@ -0,0 +1,20 @@
+/*-----------------------------------------------------------------------+
+ | net.skweez.geoclipse
+ |                                                                       |
+ | $Id$            
+ +-----------------------------------------------------------------------*/
+package net.skweez.geoclipse.map;
+
+/**
+ * 
+ * @author mks
+ * @author $Author: mks $
+ * @version $Rev$
+ * @levd.rating RED Hash:
+ */
+public abstract class Overlay {
+
+	public void draw(MapView mapView, boolean shadow) {
+	}
+
+}
